f5a16646aea34fcb19d28ababad2790532d06293
thomasnrggo <thomasnrggo@gmail.com>
2019-01-03 21:20:19 -0600

    navegacion de pantallas simple

--- src/screens/containers/movie.js
+++ src/screens/containers/movie.js
@@ -3,16 +3,30 @@ import React, { Component } from 'react';
 import MovieLayout from '../components/movie';
 import Header from '../../sections/components/header';
 import Player from '../../player/containers/player';
+import Close from '../../sections/components/close';
+import {connect} from 'react-redux';
 
 class Movie extends Component {
+  closeMovie = (props) => {
+    this.props.dispatch({
+      type: 'SET_SELECTED_MOVIE',
+      payload: {
+        movie: null,
+      }
+    })
+  }
   render() {
     return (
       <MovieLayout>
-        <Header />
+        <Header>
+          <Close
+            onPress={this.closeMovie}
+          />
+        </Header>
         <Player />
       </MovieLayout>
     );
   }
 }
 
-export default Movie;
+export default connect(null)(Movie);
---
+++ src/sections/components/close.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+const Close = (props) => (
+  <TouchableOpacity
+    onPress={props.onPress}
+    style={styles.container}>
+    <Text style={styles.button}>X</Text>
+  </TouchableOpacity>
+);
+
+export default Close;
+
+const styles = StyleSheet.create({
+  button: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  container: {
+    backgroundColor: '#14b739',
+    borderRadius: 12,
+    width: 25,
+    height: 25,
+    overflow: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
