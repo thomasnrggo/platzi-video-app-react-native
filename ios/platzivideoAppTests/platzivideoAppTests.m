0463faac358071d31d9629845581edf79e298b00
thomasnrggo <thomasnrggo@gmail.com>
2019-01-03 20:57:36 -0600

    validando pantallas

--- ios/platzivideoAppTests/platzivideoAppTests.m
+++ ios/platzivideoAppTests/platzivideoAppTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface platzivideoAppTests : XCTestCase
+@interface platziVideoAppTests : XCTestCase
 
 @end
 
-@implementation platzivideoAppTests
+@implementation platziVideoAppTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
