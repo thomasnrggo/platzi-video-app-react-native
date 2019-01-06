78383c4daa6da99e109950ee0d82772ca8a7dfc4
thomasnrggo <thomasnrggo@gmail.com>
2019-01-06 17:19:09 -0600

    Login con Switch Navigator

--- reducers/index.js
+++ reducers/index.js
@@ -3,10 +3,12 @@ import {
 } from 'redux';
 import navigation from './navigation';
 import video from './videos';
+import user from './user'
 
 const reducer = combineReducers({
   navigation,
   video,
+  user
 });
 
 export default reducer;
--- src/screens/containers/login.js
+++ src/screens/containers/login.js
@@ -18,7 +18,7 @@ class Login extends Component {
       type: 'SET_USER',
       payload: {
         token,
-        username: 'LeonidasEsteban'
+        username: 'thomasnrg'
       }
     })
     this.props.navigation.navigate('Loading');
