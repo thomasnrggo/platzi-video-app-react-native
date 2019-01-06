a5a0ece6f470bde4fea6b172844a8afed5020858
thomasnrggo <thomasnrggo@gmail.com>
2019-01-05 23:01:16 -0600

    creando componente category

--- src/app-navigation.js
+++ src/app-navigation.js
@@ -3,11 +3,19 @@ import {
 } from 'react-navigation';
 import Home from './screens/containers/home'
 import Movie from './screens/containers/movie'
+import Category from './screens/containers/category'
+import Header from './sections/components/header'
 
 const AppNavigator = createStackNavigator(
   {
     Home: Home,
     Movie: Movie,
+    Category,
+  },
+  {
+    defaultNavigationOptions: {
+      header: Header,
+    }
   }
 );
 
